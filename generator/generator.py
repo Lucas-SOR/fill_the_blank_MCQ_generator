10a4b57e288d6bf7565b8122b07bdb6d970ed720
Lucas <lucas.sor@sicara.com>
2022-08-17 14:58:18 +0200

    Added keyword extraction using yake

--- generator/generator.py
+++ generator/generator.py
@@ -1,4 +1,5 @@
 import random
+import yake
 
 from sense2vec import Sense2Vec
 from typing import List
@@ -7,6 +8,7 @@ from typing import List
 class Generator:
     def __init__(self, path_to_model: str) -> None:
         self.s2v_model = Sense2Vec().from_disk(path_to_model)
+        self.keyword_extractor = yake.KeywordExtractor()
 
     def get_most_similar_words(
         self, keyword: str, number_of_similar_words: int
@@ -42,8 +44,18 @@ class Generator:
         ]
         return random.choices(distractor_candidates_list, k=number_of_distractors)
 
+    def get_keywords(self, text: str) -> List[str]:
+        return [
+            keyword[0] for keyword in self.keyword_extractor.extract_keywords(text)
+        ][::-1]
+
 
 if __name__ == "__main__":
     g = Generator("s2v_models/s2v_old")
     word = "polar bear"
     print(g.get_distractors(word, 15))
+    print(
+        g.get_keywords(
+            "A banana is an elongated, edible fruit botanically a berry produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called plantains, distinguishing them from dessert bananas."
+        )
+    )
